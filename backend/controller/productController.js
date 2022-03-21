e883564c914046cec024a214951280b04f3beee4
mustufakhan <mohdmustufa568@gmail.com>
2022-03-21 18:16:18 +0530

    pagination and price range filter

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,30 +1,35 @@
-const Product = require('../model/productModel');
-const ErrorHandler = require('../utils/errorHandler');
-const catchAsyncError = require('../middleware/catchAsyncError');
-const ApiFeatures = require('../utils/apiFeatures')
+const Product = require("../model/productModel");
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncError = require("../middleware/catchAsyncError");
+const ApiFeatures = require("../utils/apiFeatures");
 
 //CREATE PRODUCTS --ADMIN
-exports.createProduct = catchAsyncError(async(req, res, next) => {
+exports.createProduct = catchAsyncError(async (req, res, next) => {
   const product = await Product.create(req.body);
   res.status(201).json({
     success: true,
-    product
-  })
+    product,
+  });
 });
 
 //GET ALL PRODUCTS
-exports.getAllProducts = catchAsyncError(async(req, res) => {
-  const apiFeature = await new ApiFeatures(Product.find(), req.query).search()
+exports.getAllProducts = catchAsyncError(async (req, res) => {
+  const resultPerPage = 5;
+
+  const apiFeature = await new ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage);
   // const products = await Product.find();
   const products = await apiFeature.query;
   res.status(200).json({
     success: true,
-    products
-  })
-})
+    products,
+  });
+});
 
 // get product details
-exports.getProductDetails = catchAsyncError(async(req, res, next) => {
+exports.getProductDetails = catchAsyncError(async (req, res, next) => {
   let product = await Product.findById(req.params.id);
 
   if (!product) {
@@ -33,34 +38,33 @@ exports.getProductDetails = catchAsyncError(async(req, res, next) => {
 
   res.status(200).json({
     success: true,
-    product
-  })
+    product,
+  });
+});
 
-})
-
-//UPDATE PRODUCT --ADMIN 
-exports.updateProduct =catchAsyncError( async(req, res) => {
+//UPDATE PRODUCT --ADMIN
+exports.updateProduct = catchAsyncError(async (req, res) => {
   let product = await Product.findById(req.params.id);
 
   if (!product) {
     return next(new ErrorHandler("Product not found", 404));
   }
 
-  product = await Product.findByIdAndUpdate(req.params.id, req.body,{
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidator: true,
-    useFindAndModify: false
+    useFindAndModify: false,
   });
 
   res.status(200).json({
     success: true,
-    product
-  })
-})
+    product,
+  });
+});
 
 //delete product
 
-exports.deleteProduct =catchAsyncError( async(req, res, next) => {
+exports.deleteProduct = catchAsyncError(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
 
   if (!product) {
@@ -70,6 +74,6 @@ exports.deleteProduct =catchAsyncError( async(req, res, next) => {
   await product.remove();
   res.status(200).json({
     success: true,
-    message: 'Product deleted successfully'
-  })
-})
+    message: "Product deleted successfully",
+  });
+});
